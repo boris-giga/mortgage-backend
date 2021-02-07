99470571b1989af8918595591b3ec9e54ff4cd27
boris-giga <boris.ko.uni@gmail.com>
2021-02-07 18:49:42 +0200

    updated config

--- config/db.config.js
+++ config/db.config.js
@@ -2,10 +2,10 @@
 const mysql = require('mysql');
 //local mysql db connection
 const dbConn = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'root',
-  database : 'bank_schema'
+  host     : 'us-cdbr-east-03.cleardb.com',
+  user     : 'b87f1c5b2e3543',
+  password : 'e74f5651',
+  database : 'heroku_ff5f1383f7bc448'
 });
 dbConn.connect(function(err) {
   if (err) throw err;
