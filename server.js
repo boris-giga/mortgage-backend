bd2049e19c3f9ef5551fd6e5b3861912b5c56070
boris-giga <boris.ko.uni@gmail.com>
2021-02-07 23:22:05 +0200

    cors

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express');
+const cors = require('cors')
 const bodyParser = require('body-parser');
+const path = require('path')
 // create express app
 const app = express();
+app.use(cors())
 // Setup server port
 const port = process.env.PORT || 5000;
-// Tryin' to handle CORS
-app.use( function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-  next()
-})
+// // Tryin' to handle CORS
+// app.use( function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*")
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+//   next()
+// })
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
 // parse requests of content-type - application/json
