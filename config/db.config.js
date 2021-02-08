f964e14d74a412e3d80d9d804a4661701e4be1ab
boris-giga <boris.ko.uni@gmail.com>
2021-02-08 21:59:47 +0200

    handling errors

--- config/db.config.js
+++ config/db.config.js
@@ -1,14 +1,14 @@
 'use strict';
 const mysql = require('mysql');
 //local mysql db connection
-const dbConn = mysql.createConnection({
+const dbConn = mysql.createPool({
   host     : 'us-cdbr-east-03.cleardb.com',
   user     : 'b87f1c5b2e3543',
   password : 'e74f5651',
   database : 'heroku_ff5f1383f7bc448'
 });
-dbConn.connect(function(err) {
-  if (err) throw err;
-  console.log("Database Connected!");
-});
+// dbConn.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Database Connected!");
+// });
 module.exports = dbConn;
